2c5ce274077d8d5f9ed9e89c7b1e36c02eb8e1cf
Brendan J. Herger <b@revup.com>
2015-03-06 11:06:03 -0800

    including logging

--- names_to_ethnicities.py
+++ names_to_ethnicities.py
@@ -3,15 +3,10 @@
 coding=utf-8
 """
 import argparse
+import logging
 
 import pandas as pd
 import nameparser
-import sys
-
-
-def normalize_pct(input):
-    if isinstance(input, ):
-        print 'hi'
 
 
 def create_ethnicity_df(data_path='data/app_c.csv'):
@@ -28,6 +23,7 @@ def create_ethnicity_df(data_path='data/app_c.csv'):
     # Change name from index back to column, rename
     raw_names_df = raw_names_df.reset_index()
 
+    # Rename columns to human readable values
     rename_dict = {'name': 'last', 'pctwhite': 'white', 'pctblack': 'black',
                    'pctapi': 'asian_pac_islander', 'pctaian': 'american '
                                                               'indian',
@@ -51,7 +47,8 @@ def create_names_df(data_path='test_data/test_names.txt'):
 
     # Convert to DataFrame
     names_df = pd.DataFrame(names_list)
-    # names_df = names_df[['last']]
+
+    # Uppercase to match Census data
     names_df['last'] = names_df['last'].apply(lambda x: x.upper())
 
     return names_df
@@ -67,7 +64,7 @@ def normalize_linked_in(
 
 
 def sum_ethnicity_from_file(file_path):
-
+    logging.info('Begin sum_ethnicity_from_file')
     # Create normalized ethnicity lookup DataFrame
     ethnicity_df = create_ethnicity_df()
 
@@ -77,10 +74,13 @@ def sum_ethnicity_from_file(file_path):
     # Merge
     combined_df = pd.merge(names_df, ethnicity_df, how='left', on='last')
 
+    logging.info('End sum_ethnicity_from_file')
+
     # Output
     return combined_df.mean()
 
 def sum_ethnicity_from_df(names_df, lastname_column='last'):
+    logging.info('Begin sum_ethnicity_from_df')
     # Create normalized ethnicity lookup DataFrame
     ethnicity_df = create_ethnicity_df()
 
@@ -91,11 +91,13 @@ def sum_ethnicity_from_df(names_df, lastname_column='last'):
     # Merge
     combined_df = pd.merge(names_df, ethnicity_df, how='left', on='last')
 
+    logging.info('End sum_ethnicity_from_df')
     # Output
     return combined_df.mean()
 
 
 def main():
+    logging.info('Begin Main')
     parser = argparse.ArgumentParser(description='Script to empirially '
                                                'deterime ethnic makeup of a '
                                                'group of names')
@@ -107,9 +109,7 @@ def main():
     args = parser.parse_args()
 
     print sum_ethnicity_from_file(args.data_path)
-
-
-
+    logging.info('End Main')
 
 
 if __name__ == '__main__':
